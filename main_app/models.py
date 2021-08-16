6bab9aaafd106cf3609d883f7b1b818c18390846
Kevin Ayala <kevinayala6969@gmail.com>
2021-08-16 16:08:34 -0700

    adds create, update, and delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Card(models.Model):
@@ -7,6 +8,9 @@ class Card(models.Model):
     status = models.CharField(max_length=100)
     condition = models.CharField(max_length=100)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'card_id': self.id})
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Card
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def home(request):
     return HttpResponse('<h1>Home Page</h1>')
@@ -16,3 +16,15 @@ def cards_index(request):
 def cards_detail(request, card_id):
     card = Card.objects.get(id=card_id)
     return render(request, 'cards/detail.html', {'card': card})
+
+class CardCreate(CreateView):
+    model = Card
+    fields = '__all__'
+
+class CardUpdate(UpdateView):
+    model = Card
+    fields = ['rarity', 'status', 'condition']
+
+class CardDelete(DeleteView):
+    model = Card
+    success_url = '/cards/'
